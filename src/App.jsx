9d35c65a3f32816eacc94227e7b9681db45f4fe1
gsfarfan <gustavofarfannn@gmail.com>
2024-08-20 00:40:42 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './components/Header/Header';
-
+import Products from './components/Products/products';
 function App() {
   return (
     <div>
        <Header />
+       <Products />
     </div>
   );
 }
